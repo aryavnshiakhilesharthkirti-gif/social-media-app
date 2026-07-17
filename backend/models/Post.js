04dac821b19796393ab7c871548edca0d8905580
aryavnshiakhilesharthkirti-gif <aryavnshiakhilesharthkirti@gmail.com>
2026-07-17 19:42:15 +0530

    Define Post schema with author, content, and comments

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,1 +1,37 @@
+const mongoose = require('mongoose');
 
+const PostSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  likes: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }],
+  comments: [{
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    text: String,
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }]
+}, { timestamps: true });
+
+PostSchema.index({ createdAt: -1 });
+
+module.exports = mongoose.model('Post', PostSchema);
